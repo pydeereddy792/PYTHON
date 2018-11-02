bcfd6f769850bcf6f0bb2fc6ce0c2f348e54e660
pydeereddy792 <43162700+pydeereddy792@users.noreply.github.com>
2018-11-02 16:26:47 +0530

    Add files via upload

---
+++ exil.py
@@ -0,0 +1,16 @@
+import matplotlib. pyplot as plt 
+import csv 
+x=[]
+y=[]
+with open('/home/cl302/deepthireddy/n.csv','r')as csvfile:
+	plots= csv.reader(csvfile,delimiter=',')
+	for row in plots:
+ 		x.append(int(row[0]))
+ 		y.append(int(row[1]))
+
+plt.plot(x,y, label='loaded from file!')
+plt.xlabel ('x axis')
+plt.ylabel ('y axis')
+plt.title('my test result')
+plt.legend()
+plt.show()
