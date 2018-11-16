e078bd2c4ca113d3aecf824ace34b070c9f1526f
pydeereddy792 <43162700+pydeereddy792@users.noreply.github.com>
2018-11-16 16:27:42 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,16 @@
+#wite a program to print a function of addition and subtraction
+
+a=10
+b=20
+def add():
+	return a+b
+
+def sub():
+	print(a-b)
+	return
+
+result=add()
+result=sub()
+print(result) 
+
+
